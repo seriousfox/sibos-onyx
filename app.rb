34ebd04370acadcc9d0c0ec725c271d7002cb989
Linnea Strid <linnea@seriousfox.co.uk>
2014-08-08 12:45:55 +0100

    adding partial rendering and basic social meta tags

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 Bundler.require
 
 require 'sinatra/asset_pipeline'
+require 'sinatra/partial'
 
 class App < Sinatra::Base
   configure do
@@ -10,9 +11,12 @@ class App < Sinatra::Base
   set :assets_precompile, %w(application.js libs.js application.css bootstrap.css *.png *.jpg *.svg)
   register Sinatra::AssetPipeline
 
+  register Sinatra::Partial
+  set :partial_template_engine, :erb
+
   def initialize
     super
-    
+
   end
 
   # Define urls like this, erb :file links to /views/file.erb
