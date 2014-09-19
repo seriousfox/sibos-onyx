b393fc6f31f05e748c4eb813dfd93cd1251ffe5d
Josh Crowder <josh@seriousfox.co.uk>
2014-09-19 16:50:34 +0100

    payment rtransfer

--- assets/javascripts/libs.js
+++ assets/javascripts/libs.js
@@ -1,5 +1,6 @@
 // Include the libraries you need in here
 //= require libs/jquery.min
+//= require libs/jquery-ui
 //= require libs/underscore-min
 //= require libs/jquery.pjax
 //= require libs/headtrackr
