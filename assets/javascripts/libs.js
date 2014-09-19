9aebf48376cf44acf753ce12ea6732561317b5c9
Josh Crowder <josh@seriousfox.co.uk>
2014-09-19 17:44:11 +0100

    Optimising images

--- assets/javascripts/libs.js
+++ assets/javascripts/libs.js
@@ -1,6 +1,7 @@
 // Include the libraries you need in here
 //= require libs/jquery.min
 //= require libs/jquery-ui
+//= require libs/jquery-ui.touch-punch
 //= require libs/underscore-min
 //= require libs/jquery.pjax
 //= require libs/headtrackr
